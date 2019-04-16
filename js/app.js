a5fdabc649a68eb3cfc35f04e3b95f5df1d50740
thewr <36010296+thewr@users.noreply.github.com>
2019-04-16 07:09:28 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,9 +12,6 @@ $(function(){
 
 // creates a <table> element and a <tbody> element
 const itemList = document.querySelector('#item-list');
-let tblBody = document.createElement("tbody");    
-// get the reference for the body
-var body = document.getElementsByTagName("body")[0];
 
 // create form element
 const form = document.querySelector('#add-item-form');
@@ -22,31 +19,30 @@ const form = document.querySelector('#add-item-form');
 
 // create element & render cafe
 function renderCafe(doc){
-    
-    // creates a table row
-    let row = document.createElement("tr");
-    itemList.setAttribute('data-id', doc.id);
-    let name = document.createElement('td'); 
-    let amount = document.createElement('td');
-    let subject = document.createElement('td');
-    name.textContent = doc.data().name;
-    amount.textContent = doc.data().amount;
-    subject.textContent = doc.data().subject;
-    
-    // append row
-    row.appendChild(name);
-    row.appendChild(amount);
-    row.appendChild(subject);
-    
-    // add the row to the end of the table body  
-    tblBody.appendChild(row);
-    
-    // put the <tbody> in the <table>
-    itemList.appendChild(tblBody);
-  // appends <table> into <body>
- //   body.appendChild(itemList);
-  // sets the border attribute of tbl to 2;
-   //itemList.setAttribute("border", "2");
+  // create list document object
+  let li = document.createElement('li');
+  li.setAttribute('data-id', doc.id);  //Each document gets an id.
+
+  //let date = document.createElement('span');
+  //let label = document.createElement('span');//.setAttribute("style","display: inline-block;");
+  //let fname = document.createElement('span');
+  //let lname = document.createElement('span');
+  let name = document.createElement('span');
+  let amount = document.createElement('span');
+  let info = document.createElement('span');
+
+  name.textContent = doc.data().name;
+  amount.textContent = doc.data().amount;
+  info.textContent = doc.data().info;
+ 
+  // append list
+  li.appendChild(name);
+  li.appendChild(amount);
+  li.appendChild(info);
+
+
+  // put the <tbody> in the <table>
+  itemList.appendChild(li);
 }
 
 // getting data
