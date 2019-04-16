9d1b28684f2b161461980a21aa6c349add789566
thewr <36010296+thewr@users.noreply.github.com>
2019-04-16 07:11:18 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,25 @@
-$(function(){
-  $(".content").hide();
-  $("#add_item_button").click(function(){
-    $(".content").hide().fadeIn(1000);
-   // $("#add_item_button").hide();
-  });
-  $('#item_submit').click(function(){
-    $(".content").show().fadeOut(1000);
-   // $("add_item_button").show();
-  });
-});
-
 // creates a <table> element and a <tbody> element
 const itemList = document.querySelector('#item-list');
-
 // create form element
 const form = document.querySelector('#add-item-form');
-
 
 // create element & render cafe
 function renderCafe(doc){
   // create list document object
   let li = document.createElement('li');
   li.setAttribute('data-id', doc.id);  //Each document gets an id.
-
-  //let date = document.createElement('span');
-  //let label = document.createElement('span');//.setAttribute("style","display: inline-block;");
-  //let fname = document.createElement('span');
-  //let lname = document.createElement('span');
   let name = document.createElement('span');
   let amount = document.createElement('span');
-  let info = document.createElement('span');
+  let subject = document.createElement('span');
 
   name.textContent = doc.data().name;
   amount.textContent = doc.data().amount;
-  info.textContent = doc.data().info;
+  subject.textContent = doc.data().subject;
  
   // append list
   li.appendChild(name);
   li.appendChild(amount);
-  li.appendChild(info);
+  li.appendChild(subject);
 
 
   // put the <tbody> in the <table>
