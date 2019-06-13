a476cea08ca6fc729efc9a09b586420b1a71fd4e
thewr <36010296+thewr@users.noreply.github.com>
2019-06-13 18:07:06 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -137,9 +137,9 @@ function renderDB(doc){
   // create list document elements
   let li = document.createElement('li');
   li.setAttribute('data-id', doc.id);  //Each document gets an id.
-  let name = document.createElement('span');
+  let name = document.createElement('span').classList.add("title");
   //name.style.cssText = "padding: 3px 0px 3px 12px; font-size: 16px; border-bottom: 2px solid black";
-  name.classList.add("title");
+  //name.classList.add("title");
   let wordsA = document.createElement('span');
   wordsA.style.cssText = "padding: 3px 0px 3px 12px; font-size: 16px;";
   let wordsB = document.createElement('span');
