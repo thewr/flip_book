f171fc8aca1f7ab9d8a52b26f060adbd67fa35a3
thewr <36010296+thewr@users.noreply.github.com>
2019-07-02 10:04:40 -0400

    Create tracker.js

---
+++ js/tracker.js
@@ -0,0 +1,25 @@
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
+console.log(formatDate(new Date()));  // show current date-time in console
+
+Date.prototype.addHours = function(h) {
+  this.setTime(this.getTime() + (h*60*60*1000));
+  return this;
+}
+
+var d = new Date().addHours(10).toLocaleString();
+document.getElementById("demo").innerHTML = d;
+</script>
