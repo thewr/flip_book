6bda27864e8d2303cde1099f77f6a2c3fd870931
thewr <36010296+thewr@users.noreply.github.com>
2020-02-18 08:14:06 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -323,7 +323,7 @@ $(function(){
 		display_min();
 
 		clearForm();
-		//refresh();
+		refresh();
 	});
 
 
@@ -380,10 +380,11 @@ var selectedListener = function(e) {
 		});
 
 
-		//$('#delete_item').click(function(){
-		//	if(!element.classList.contains('selected')) return;
-		//	ref.delete();
-		//});
+		$('#delete_item').click(function(){
+			if(!element.classList.contains('selected')) return;
+			ref.delete();
+			refresh();
+		});
 
 
 };
